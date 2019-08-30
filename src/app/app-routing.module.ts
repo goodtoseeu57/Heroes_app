8413ff1f8532c7605cd3030d73d8c9f826e11536
goodtoseeu57 <goodtoseeu57@gmail.com>
2019-08-30 13:50:54 +0300

    usual Angular app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeroesComponent} from './heroes/heroes.component';
+
+// const routes :
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
